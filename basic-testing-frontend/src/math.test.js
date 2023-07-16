f3c894151ac160359d7cd4ea686c2ce8abc3ec94
Verner Ravil <werner.ravil@gmail.com>
2023-07-16 20:46:28 +0500

    Fixed AAA patter in math test

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -5,14 +5,14 @@ import { add } from "./math";
 it("should summarize all number values in an array", () => {
     // Arrange
     const numbers = [1, 2, 3];
-    const expectedResult = numbers.reduce(
-        (prevValue, curValue) => prevValue + curValue,
-        0,
-    );
 
     // Act
     const result = add(numbers);
 
     // Assert
+    const expectedResult = numbers.reduce(
+        (prevValue, curValue) => prevValue + curValue,
+        0,
+    );
     expect(result).toBe(expectedResult);
 });
