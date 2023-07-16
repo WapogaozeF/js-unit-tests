6015052dd8076139fe56ba6f499b41d878679ce9
Verner Ravil <werner.ravil@gmail.com>
2023-07-16 20:45:23 +0500

    Added math test

---
+++ basic-testing-frontend/src/math.test.js
@@ -0,0 +1,18 @@
+import { expect, it } from "vitest";
+
+import { add } from "./math";
+
+it("should summarize all number values in an array", () => {
+    // Arrange
+    const numbers = [1, 2, 3];
+    const expectedResult = numbers.reduce(
+        (prevValue, curValue) => prevValue + curValue,
+        0,
+    );
+
+    // Act
+    const result = add(numbers);
+
+    // Assert
+    expect(result).toBe(expectedResult);
+});
