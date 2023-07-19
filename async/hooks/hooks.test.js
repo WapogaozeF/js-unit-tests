7b1fe0c39699011e0cbc035a5f904f4223958068
Verner Ravil <werner.ravil@gmail.com>
2023-07-19 19:51:30 +0500

    Added concurrent to hooks test and remove not neaded imports statement

--- async/hooks/hooks.test.js
+++ async/hooks/hooks.test.js
@@ -1,4 +1,4 @@
-import { it, expect, beforeAll, beforeEach, afterEach, afterAll } from "vitest";
+import { it, expect, describe, beforeEach } from "vitest";
 
 import { User } from "./hooks";
 
@@ -9,30 +9,32 @@ beforeEach(() => {
     user = new User(testEmail);
 });
 
-it("should update the email", () => {
-    const newTestEmail = "test2@test.com";
+describe.concurrent("User", () => {
+    it("should update the email", () => {
+        const newTestEmail = "test2@test.com";
 
-    user.updateEmail(newTestEmail);
+        user.updateEmail(newTestEmail);
 
-    expect(user.email).toBe(newTestEmail);
-});
+        expect(user.email).toBe(newTestEmail);
+    });
 
-it("should have an email property", () => {
-    expect(user).toHaveProperty("email");
-});
+    it("should have an email property", () => {
+        expect(user).toHaveProperty("email");
+    });
 
-it("should store the provided email value", () => {
-    expect(user.email).toBe(testEmail);
-});
+    it("should store the provided email value", () => {
+        expect(user.email).toBe(testEmail);
+    });
 
-it("should clear the email", () => {
-    user.clearEmail();
+    it("should clear the email", () => {
+        user.clearEmail();
 
-    expect(user.email).toBe("");
-});
+        expect(user.email).toBe("");
+    });
 
-it("should still have an email property after clearing the email", () => {
-    user.clearEmail();
+    it("should still have an email property after clearing the email", () => {
+        user.clearEmail();
 
-    expect(user).toHaveProperty("email");
+        expect(user).toHaveProperty("email");
+    });
 });
