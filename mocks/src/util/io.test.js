78ad6933e49f3f2cc4481f4179b81e733920ee34
Verner Ravil <werner.ravil@gmail.com>
2023-07-19 20:55:57 +0500

    Added mock instead of original modules

--- mocks/src/util/io.test.js
+++ mocks/src/util/io.test.js
@@ -4,12 +4,21 @@ import { promises as fs } from "fs";
 import writeData from "./io";
 
 vi.mock("fs");
+vi.mock("path", () => {
+    return {
+        default: {
+            join: (...args) => {
+                return args[args.length - 1];
+            }
+        }
+    };
+});
 
 it("should execute the writeFile method", () => {
-    const testDate = "Test";
+    const testData = "Test";
     const testFilename = "test.txt";
 
-    writeData(testDate, testFilename);
+    writeData(testData, testFilename);
 
-    expect(fs.writeFile).toBeCalled();
+    expect(fs.writeFile).toBeCalledWith(testFilename, testData);
 });
